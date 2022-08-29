136c937aa2afbe1ea01ee6a1122e3d0053f9d66d
Tiebing Zhang <91164682+gotodd@users.noreply.github.com>
2022-08-28 20:59:14 -0700

    baby bot basic drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,11 +4,13 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -17,10 +19,10 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  * directory.
  */
 public class Robot extends TimedRobot {
-  private final PWMSparkMax m_leftDrive = new PWMSparkMax(0);
-  private final PWMSparkMax m_rightDrive = new PWMSparkMax(1);
+  private final WPI_TalonFX m_leftDrive = new WPI_TalonFX(11);
+  private final WPI_TalonFX m_rightDrive = new WPI_TalonFX(12);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftDrive, m_rightDrive);
-  private final Joystick m_stick = new Joystick(0);
+  private final XboxController m_stick = new XboxController(0);
   private final Timer m_timer = new Timer();
 
   /**
@@ -47,20 +49,28 @@ public class Robot extends TimedRobot {
   public void autonomousPeriodic() {
     // Drive for 2 seconds
     if (m_timer.get() < 2.0) {
-      m_robotDrive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+      m_robotDrive.arcadeDrive(0.3, 0.0); // drive forwards half speed
     } else {
       m_robotDrive.stopMotor(); // stop robot
     }
   }
 
-  /** This function is called once each time the robot enters teleoperated mode. */
-  @Override
+  /** This function is called once each time the robot enters teleoperated mode. */ @Override
   public void teleopInit() {}
 
   /** This function is called periodically during teleoperated mode. */
   @Override
   public void teleopPeriodic() {
-    m_robotDrive.arcadeDrive(m_stick.getY(), m_stick.getX());
+    m_robotDrive.arcadeDrive(m_stick.getLeftY()*0.3, m_stick.getLeftX()*0.3);
+    //m_leftDrive.set(ControlMode.PercentOutput,m_stick.getLeftY()*0.3);
+    
+    if (m_stick.getAButton()){
+      System.err.println("A pressed");
+      m_rightDrive.set(ControlMode.Velocity,m_stick.getRightY()*102400);
+    }else{
+      System.out.println("A not pressed");
+    }
+
   }
 
   /** This function is called once each time the robot enters test mode. */
